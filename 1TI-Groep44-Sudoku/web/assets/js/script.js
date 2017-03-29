51c8db3e0e8c55d27f5b702c4a90a732c6cda533
FlorisVandenBussche <florisvandenbussche97@gmail.com>
2017-03-29 12:01:13 +0200

    themafunction improved

--- 1TI-Groep44-Sudoku/web/assets/js/script.js
+++ 1TI-Groep44-Sudoku/web/assets/js/script.js
@@ -1,25 +1,7 @@
-var makebackgroundgreen = function () {
-    document.body.style.background = "";
-    document.body.style.background = "green";
+var changeBackground = function () {
+    $("body").css('background-color', $(this).val());
 };
-var makebackgroundred = function () {
-    document.body.style.background = "";
-    document.body.style.background = "red";
-};
-var makebackgroundorange = function () {
-    document.body.style.background = "";
-    document.body.style.background = "orange";
 
-};
-var makebackgroundyellow = function () {
-    document.body.style.background = "";
-    document.body.style.background = "yellow";
-};
 $(document).ready(function () {
-    //Deel 1
-    //ok
-    $('input[value="green"]').on('click', makebackgroundgreen);
-    $('input[value="red"]').on('click', makebackgroundred);
-    $('input[value="orange"]').on('click', makebackgroundorange);
-    $('input[value="yellow"]').on('click', makebackgroundyellow);
+    $('input').on('click', changeBackground);
 });
